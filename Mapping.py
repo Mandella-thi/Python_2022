b0a4155da6bd7587d6076c7644239b3848afc0d0
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2022-11-21 14:03:52 -0300

    "Mapping2"

--- Mapping.py
+++ Mapping.py
@@ -5,3 +5,8 @@ my_list = map(addition, new_list)
 print(my_list)
 for item in my_list:
     print(item)
+def adding(x,y):
+    return x+y
+my_list= map(adding,('apple ','watermelon '),('banana','fig'))
+print(tuple(my_list))
+print(type(my_list))
