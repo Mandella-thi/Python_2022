97cfacd25b61662d2af225867aa0141e699b1b69
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2022-11-21 12:55:37 -0300

    "Zip"

--- zip.py
+++ zip.py
@@ -1,7 +1,12 @@
-x ={}
-for i in zip([1,2,3],['x','y','z']):
-    global x
+h ={}
+#for i in zip([1,2,3],['x','y','z']):
+    #global h
 
 
-    print(i)
-print(x)
+ #   print(i)
+#print(x)
+
+#unzipping
+unzipping = zip([10,20,30],['x','y','z'],['$','@','*'])
+x,y,z = zip(*unzipping)
+print(x,y,z)
