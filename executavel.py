244bdbe2b544a3fce15a3f98b67406b02416bc5d
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2022-11-24 13:04:26 -0300

    "Executável3"

--- executavel.py
+++ executavel.py
@@ -1,3 +1,4 @@
+from math import *
 exec("print('hi,there!')")
 new_code ='''x= 10
 y=20
@@ -5,3 +6,8 @@ print('x+y=', x+y)'''
 exec(new_code)
 insert_code = input("Enter python code here: ")
 exec(insert_code)
+exec('print(abs(-10))')
+exec('print(dir())')
+code = '''x= sqrt(16)
+print(x)'''
+exec(code, {'sqrt': sqrt})
