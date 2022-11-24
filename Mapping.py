6844119288ef2b59d14fd573a0fb569d3ae72619
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2022-11-24 10:31:53 -0300

    "Mapping3"

--- Mapping.py
+++ Mapping.py
@@ -10,3 +10,13 @@ def adding(x,y):
 my_list= map(adding,('apple ','watermelon '),('banana','fig'))
 print(tuple(my_list))
 print(type(my_list))
+
+new_list =[5,12,20,25,30]
+cube_n =map(lambda n: n*n*n, new_list)
+print(cube_n)
+print(tuple(cube_n))
+number1 =[10,20,30]
+number2 =[40,50,60]
+new_result = map(lambda a,b: a*b, number1, number2)
+print(new_result)
+print(list(new_result))
