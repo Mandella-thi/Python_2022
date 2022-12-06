8a391fb2d877248f31ddc620c54433a1696dcd13
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2022-12-06 10:50:08 -0300

    "decorators 2"

--- Decorators.py
+++ Decorators.py
@@ -10,3 +10,15 @@ def adding(num):
     return total
 
 print(adding(12))
+def calling(new_func):
+    new_number = 12
+    return new_func(new_number)
+
+print(calling(adding))
+
+def hello():
+    def greeting():
+        return "Hi there"
+    return greeting
+new_greet =hello()
+print(new_greet())
